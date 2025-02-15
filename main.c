d11e0c2e7e11eb1c0bfed050308c7fe142f30d46
BartłomiejGazduslki <heluios05@gmail.com>
2025-02-15 23:42:44 +0100

    port_check function was added. Daemon is cheking to see if port is free

--- main.c
+++ main.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdbool.h>
 #include "daemonProcess.h"
+
+#ifndef NPORTS
+#define NPORTS 65535
+#endif
+
+const int nports = NPORTS;
 
 // To Do list
 // - writing outputs to log file
-// - cheking ports
 // - header file with port acvivity structure
-// - dynamic buffer between ports and log file
 // - setting dynamic cheking of ports
 
-/*
- * main function of daemon
- */
+bool port_check(int port_number){
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0){
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port_number);
+
+    if(bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0){
+        if(errno == EADDRINUSE){
+            close(sock);
+            return false;
+        }
+        else{
+            close(sock);
+            exit(EXIT_FAILURE);
+        }
+    }
+    close(sock);
+    return true;
+}
+
 int main(int argc, char *argv[]){
     /* initalizing a daemon process */
-    struct daemonProcess process = daemonInit();
+    //struct daemonProcess process = daemonInit();
     
-    printf("child process has now his own group %d\n", process.sid);
+    //printf("child process has now his own group %d\n", process.sid);
 
+    for(int port_number = 1; port_number < nports; port_number++){
+        if(port_check(port_number)){
+            //printf("%d port free\n", port_number);
+        }
+        else{
+            printf("%d port is used\n", port_number);
+        }
+    }
 
     return 0;
 }
